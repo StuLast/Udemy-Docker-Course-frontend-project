9737f563c1a5f8553ef0f86d0ffbd768c891ec26
Stu Last <stuart@last-family.co.uk>
2019-05-27 02:14:20 +0100

    Setup docker-composer.yml with shared volume config

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 # Udemy-Docker-Course-frontend-project
 Udemy Docker Course Front End Project
+=======
+# FRONTEND
+
+## DOCKER
+>>>>>>> Setup docker-composer.yml with shared volume config
--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Felicitations and pericombobulations
         </a>
       </header>
     </div>
